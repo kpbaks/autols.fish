f6dcb3d7b694a6fe82ab3fa129a84d44fa84672f
Kristoffer Sørensen <kristoffer.pbs@gmail.com>
2020-07-29 00:21:02 +0200

    autols-toggle seems to work correctly, added --no-files to tab-completion file

--- completions/autols-toggle.fish
+++ completions/autols-toggle.fish
@@ -1,3 +1,6 @@
+# Disable file tab-completion
+complete -c autols-toggle -f
+
 set --local autols_commands "on off"
 
 complete -c autols-toggle -n "not __fish_seen_subcommand_from $autols_commands" -a "on" -d "Toggle autols functionality on"
