28fc3459960b60933385f028d58e2d762ae9d796
Kristoffer Sørensen <kristoffer.pbs@gmail.com>
2020-07-28 23:38:39 +0200

    testing if function --on-variable PWD works

--- functions/_autols.fish
+++ functions/_autols.fish
@@ -1,13 +1,17 @@
-function _autols --on-event fish_postexec
-    set --query _autols_previous_dir; or set -g _autols_previous_dir (dirs)
-    set --query _autols_current_dir; or set -g _autols_current_dir (dirs)
-
-    set -g _autols_current_dir (dirs)
-
-    if test ! "$_autols_previous_dir" = "$_autols_current_dir"
-        ls
-        set -g _autols_previous_dir (dirs)
-    end
+function _autols --on-variable PWD
+    ls
 end
+
+# function _autols --on-event fish_postexec
+#     set --query _autols_previous_dir; or set -g _autols_previous_dir (dirs)
+#     set --query _autols_current_dir; or set -g _autols_current_dir (dirs)
+
+#     set -g _autols_current_dir (dirs)
+
+#     if test ! "$_autols_previous_dir" = "$_autols_current_dir"
+#         ls
+#         set -g _autols_previous_dir (dirs)
+#     end
+# end
 
 
