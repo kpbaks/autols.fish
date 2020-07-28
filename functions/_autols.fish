881116432913a55d478ca623babcdd81259adf22
Kristoffer Sørensen <kristoffer.pbs@gmail.com>
2020-07-28 23:30:59 +0200

    initial barebones implementation

---
+++ functions/_autols.fish
@@ -0,0 +1,13 @@
+function _autols --on-event fish_postexec
+    set --query _autols_previous_dir; or set -g _autols_previous_dir (dirs)
+    set --query _autols_current_dir; or set -g _autols_current_dir (dirs)
+
+    set -g _autols_current_dir (dirs)
+
+    if test ! "$_autols_previous_dir" = "$_autols_current_dir"
+        ls
+        set -g _autols_previous_dir (dirs)
+    end
+end
+
+
---
+++ functions/autols-toggle.fish
@@ -0,0 +1,3 @@
+function autols-toggle --description "toggle autols"
+    
+end
