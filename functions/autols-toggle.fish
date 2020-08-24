491a468b172c20ed96514d89c1b7d28871f14efb
Kristoffer Sørensen <kristoffer.pbs@gmail.com>
2020-08-24 16:25:08 +0200

    adaskdkk

--- functions/autols-toggle.fish
+++ functions/autols-toggle.fish
@@ -14,7 +14,20 @@ function autols-toggle --description "toggle autols"
                 set --query _autols_toggle_status; and set --erase _autols_toggle_status
                 echo -e "\tautols has been turned off!"
             case "-h" "--help"
-                echo "help message"
+                _autols::help
         end 
     end
 end
+
+function _autols::help --description "print help message for autols-toggle"
+    echo -e "usage: autols-toggle on\tturn autols on"
+    echo -e "\tautols-toggle off\tturn autols off"
+    echo -e "autols-toggle -h|--help\t Show this help message"
+
+    # set -l author "Kristoffer Sørensen" 
+    # set -l repo "https://github.com/Kristoffer-PBS/autols-fish"
+    # echo -e "autols-fish - a small plugin that automatically invokes ls whenever you enter a new directory.\n"
+    # echo -e "author:\t$author\n"
+    # echo -e "repo:\t$repo\n\n"
+end
+
