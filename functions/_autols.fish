6f8e2e9bff20c44c6045d8b59dd417671cd17fa9
Kristoffer Sørensen <kristoffer.pbs@gmail.com>
2020-09-12 23:15:26 +0200

    added extra if statement to fix previous issue

--- functions/_autols.fish
+++ functions/_autols.fish
@@ -7,14 +7,6 @@ function _autols --on-event fish_postexec
 
     set -g _autols_current_dir (dirs)
 
-    # query ~/.config/fish/fish_variables to see if the environment variable
-    # '_autols_toggle_status' has been defined or not. It is the variable
-    # which control whether or not autols is toggled on or off. It
-    # can be set and unset respectively with autols_toggle [on | off].
-    # If not set, then simply return since that is interpreted as autols.fish
-    # being toggled off.
-    set --query _autols_toggle_status; or return 
-    
     # Test if the previous directory is equal to the current one, which is
     # set with the end of a fish prompt.
     # If they are not equal, then the shell must have switched to another directory
@@ -22,7 +14,15 @@ function _autols --on-event fish_postexec
     # If they are equal then the shell must not have switched to another directory,
     # and there is no reason to execute ls.
     if test ! "$_autols_previous_dir" = "$_autols_current_dir"
-        ls
-        set -g _autols_previous_dir (dirs)
+        set -g _autols_previous_dir (dirs)  
+        # query ~/.config/fish/fish_variables to see if the environment variable
+        # '_autols_toggle_status' has been defined or not. It is the variable
+        # which control whether or not autols is toggled on or off. It
+        # can be set and unset respectively with autols_toggle [on | off].
+        # If not set, then simply return since that is interpreted as autols.fish
+        # being toggled off.
+        if set --query _autols_toggle_status 
+            ls
+        end
     end
 end
