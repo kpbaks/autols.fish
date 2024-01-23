25947841cfef297cf8320304c0295019da60543c
kpbaks <kristoffer.pbs@tuta.io>
2024-01-23 12:22:53 +0100

    feat: ignore directory patterns using `autols add`

--- completions/autols.fish
+++ completions/autols.fish
@@ -3,10 +3,15 @@ set -l c complete -c autols
 $c -f # Disable file completion
 
 $c -s h -l help
-set -l subcommands on off status toggle
+set -l subcommands on off status toggle add list ls remove rm
 
 set -l cond "not __fish_seen_subcommand_from $subcommands"
 $c -n $cond -a on
 $c -n $cond -a off
 $c -n $cond -a status
 $c -n $cond -a toggle
+$c -n $cond -a add
+$c -n $cond -a list
+$c -n $cond -a ls
+$c -n $cond -a remove
+$c -n $cond -a rm
--- functions/autols.fish
+++ functions/autols.fish
@@ -13,9 +13,9 @@ function autols -d "Interact with autols.fish"
     set -l reset (set_color normal)
     set -l red (set_color red)
     set -l green (set_color green)
+    set -l yellow (set_color yellow)
+    set -l blue (set_color blue)
     if set --query _flag_help
-        set -l blue (set_color blue)
-        set -l yellow (set_color yellow)
         set -l bold (set_color --bold)
         set -l option_color (set_color $fish_color_option)
         set -l param_color (set_color $fish_color_param)
@@ -26,32 +26,47 @@ function autols -d "Interact with autols.fish"
         printf "\t%s%s%s <command> [options]\n" (set_color $fish_color_command) (status function) $reset >&2
         printf "\n" >&2
         printf "%sCOMMANDS%s\n" $yellow $reset >&2
-        printf "\t%sstatus%s\treturns 0 if autols.fish is enabled, 1 otherwise\n" $param_color $reset >&2
-        printf "\t%son%s\t%senables%s autols.fish\n" $param_color $reset $green $reset >&2
-        printf "\t%soff%s\t%sdisables%s autols.fish\n" $param_color $reset $red $reset >&2
-        printf "\t%stoggle%s\ttoggles autols.fish between on and off\n" $param_color $reset >&2
+        printf "\t%sstatus%s\t\treturns 0 if autols.fish is enabled, 1 otherwise\n" $param_color $reset >&2
+        printf "\t%son%s\t\t%senables%s autols.fish\n" $param_color $reset $green $reset >&2
+        printf "\t%soff%s\t\t%sdisables%s autols.fish\n" $param_color $reset $red $reset >&2
+        printf "\t%stoggle%s\t\ttoggles autols.fish between on and off\n" $param_color $reset >&2
+        printf "\t%sadd%s\t\t%sadds%s directories to the list of ignored directories\n" $param_color $reset $green $reset >&2
+        printf "\t%sremove rm%s\t%sremoves%s directories from the list of ignored directories\n" $param_color $reset $red $reset >&2
+        printf "\t%slist ls%s\t\tlists the directories that are ignored\n" $param_color $reset >&2
+
         printf "\n" >&2
         printf "%sOPTIONS%s\n" $yellow $reset >&2
         printf "\t%s-h%s, %s--help%s\tShow this help message and exit\n" $option_color $reset $option_color $reset >&2
+        printf "\n" >&2
+        printf "%sEXAMPLES%s\n" $yellow $reset >&2
+        printf "\t%s%s\n" (printf (echo "autols add '*/.git' # Ignore all */.git/ directories" | fish_indent --ansi)) $reset >&2
+        printf "\t%s%s\n" (printf (echo "autols add '*/build' # Ignore all */build/ directories" | fish_indent --ansi)) $reset >&2
+        printf "\t%s%s\n" (printf (echo "autols add ~/Pictures # Ignore $HOME/Pictures" | fish_indent --ansi)) $reset >&2
 
-    set -l github_url https://github.com/kpbaks/autols.fish
-    set -l star_symbol ⭐
-    set -l magenta (set_color magenta)
-    printf "\n" >&2
-    printf "Part of %sautols.fish%s. A plugin for the %s><>%s shell.\n" $magenta $reset $blue $reset >&2
-    printf "See %s%s%s for more information, and if you like it, please give it a %s\n" (set_color --underline cyan) $github_url $reset $star_symbol >&2
+        set -l github_url https://github.com/kpbaks/autols.fish
+        set -l star_symbol ⭐
+        set -l magenta (set_color magenta)
+        printf "\n" >&2
+        printf "Part of %sautols.fish%s. A plugin for the %s><>%s shell.\n" $magenta $reset $blue $reset >&2
+        printf "See %s%s%s for more information, and if you like it, please give it a %s\n" (set_color --underline cyan) $github_url $reset $star_symbol >&2
 
         return 0
     end
+
+    set --query AUTOLS_FISH_IGNORED_DIRS; or set --universal AUTOLS_FISH_IGNORED_DIRS # Initialize the variable if it does not exist
+    
+    set -l retv 0
 
     switch $argv[1]
         case status
             if set --query AUTOLS_FISH_DISABLED
                 printf "%sdisabled%s\n" $red $reset >&2
-                return 1
+                set retv 1
+                # return 1/
             else
                 printf "%senabled%s\n" $green $reset >&2
-                return 0
+                set retv 0
+                # return 0
             end
 
         case on
@@ -77,5 +92,38 @@ function autols -d "Interact with autols.fish"
             else
                 eval (status function) off
             end
+        case add
+            if test (count $argv) -lt 2
+                printf "%serror%s: add requires at least one argument\n" $red $reset >&2
+                return 2
+            end
+
+            for dir in $argv[2..]
+                if contains -- $dir $AUTOLS_FISH_IGNORED_DIRS
+                    printf "%swarn%s: %s is already ignored\n" $yellow $reset $dir >&2
+                    set retv 1
+                    continue
+                end
+                set --append AUTOLS_FISH_IGNORED_DIRS $dir
+            end
+        case remove rm
+            if test (count $argv) -lt 2
+                printf "%serror%s: remove requires at least one argument\n" $red $reset >&2
+                return 2
+            end
+            for dir in $argv[2..]
+                if contains --index -- $dir $AUTOLS_FISH_IGNORED_DIRS | read index
+                    set --erase AUTOLS_FISH_IGNORED_DIRS[$index]
+                else
+                    printf "%swarn%s: %s is not ignored\n" $yellow $reset $dir >&2
+                    set retv 1
+                end
+            end
+        case list ls
+            if test (count $AUTOLS_FISH_IGNORED_DIRS) -gt 0
+                printf "%s\n" $AUTOLS_FISH_IGNORED_DIRS
+            end
     end
+
+    return $retv
 end
